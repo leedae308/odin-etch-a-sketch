e076eab8550ee93f6b9983c6ccb09c5c83536848
Daehee Lee <daehee.lee308@gmail.com>
2025-02-13 00:30:58 +0900

    create 16x16 basic grid
    had to use padding 20px because it seems like there
    is no way to make the grid height: 100% and width
    equal to height. Maybe there is, but I could not
    figure out.
    
    I will ask chatGPT tomorrow.
    
    Need to work on part 3, adding hover effect.

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 //DIV Creation
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++) //for-loop to create rowsContainer;
+for (let i = 0; i < 16; i++) //for-loop to create rowsContainer;
 {
     const rowsContainer = document.createElement("div");
-    rowsContainer.classList="rowsContainer";
+    rowsContainer.classList = "rowsContainer";
 
-    for(let j = 0; j<16; j++) //stuff 16 divs (gridBoxes) in each rowsContainer;
+    for (let j = 0; j < 16; j++) //stuff 16 divs (gridBoxes) in each rowsContainer;
     {
         const gridBox = document.createElement("div");
-        gridBox.classList="gridBox"
+        gridBox.classList = "gridBox"
         rowsContainer.appendChild(gridBox);
     }
     container.appendChild(rowsContainer);
