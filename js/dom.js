a14af4ba4fae23ffd11159d0c40d58783aa48a52
Daehee Lee <daehee.lee308@gmail.com>
2025-02-13 12:38:50 +0900

    add reset grid size functionality
    i have separated the js document for it to keep dom
    elements in one file, and event elements in the other.
    
    dom file has now createNewGrid element.
    To reset the container, i used innerHTML = "", then
    I rewrite the whole grid. Thought this would be the
    easiest way to tackle this problem.

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,14 @@
 //DIV Creation
 const container = document.querySelector(".container");
+const buttonSpace=document.querySelector('.buttonSpace');
 
+const resetBtn = document.createElement("button");
+resetBtn.innerText = "Click to Re-make the Grid";
+
+buttonSpace.appendChild(resetBtn);
+
+
+//initial 16x16 grid;
 for (let i = 0; i < 16; i++) //for-loop to create rowsContainer;
 {
     const rowsContainer = document.createElement("div");
@@ -13,4 +21,23 @@ for (let i = 0; i < 16; i++) //for-loop to create rowsContainer;
         rowsContainer.appendChild(gridBox);
     }
     container.appendChild(rowsContainer);
+}
+
+
+function createNewGrid(newSize) {
+    
+    container.innerHTML="";
+    for (let i = 0; i < newSize; i++) //for-loop to create rowsContainer;
+    {
+        const rowsContainer = document.createElement("div");
+        rowsContainer.classList = "rowsContainer";
+
+        for (let j = 0; j < newSize; j++) //stuff 16 divs (gridBoxes) in each rowsContainer;
+        {
+            const gridBox = document.createElement("div");
+            gridBox.classList = "gridBox"
+            rowsContainer.appendChild(gridBox);
+        }
+        container.appendChild(rowsContainer);
+    }
 }
--- js/event.js
+++ js/event.js
@@ -1,8 +1,25 @@
 //hovering and coloring
-
 const box = document.querySelectorAll(".gridBox");
-box.forEach((gridBox)=> {
-    gridBox.addEventListener("mouseover", (event)=>{
+box.forEach((gridBox) => {
+    gridBox.addEventListener("mouseover", (event) => {
         event.target.style.background = "orange";
     });
+});
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', (e) => {
+    const newSize = prompt("Input the new Grid size [range: 1 - 100]")
+    if (newSize < 1 || newSize > 100) { alert("Invalid Input Range"); }
+    else {
+        // alert("entered"); //for debugging
+        createNewGrid(newSize);
+
+        const box = document.querySelectorAll(".gridBox");
+        box.forEach((gridBox) => {
+            gridBox.addEventListener("mouseover", (event) => {
+                event.target.style.background = "orange";
+            });
+        });
+
+    }
 });
